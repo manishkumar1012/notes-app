8f481be851b4fe2bdabd62a0a521000168748930
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-02-06 11:24:13 +0530

    Added functionality for reading a note

--- app.js
+++ app.js
@@ -1,49 +1,64 @@
-const fs = require('fs');
-const notes = require('./notes.js');
-const chalk = require('chalk');
-const yargs = require('yargs');
+const fs = require("fs");
+const notes = require("./notes.js");
+const chalk = require("chalk");
+const yargs = require("yargs");
 
 yargs.command({
-    command: 'add',
-    describe: 'Add note to the list',
-    builder: {
-        title: {
-            describe: 'Title for the note',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Body of the note',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  describe: "Add note to the list",
+  builder: {
+    title: {
+      describe: "Title for the note",
+      demandOption: true,
+      type: "string",
     },
-    handler(argv) {
-        notes.add(argv.title, argv.body);
-    }
+    body: {
+      describe: "Body of the note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.add(argv.title, argv.body);
+  },
 });
 
 yargs.command({
-    command: 'remove',
-    describe: 'Removes a note of the specified title',
-    builder: {
-        title: {
-            describe: 'title to remove',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "remove",
+  describe: "Removes a note of the specified title",
+  builder: {
+    title: {
+      describe: "title to remove",
+      demandOption: true,
+      type: "string",
     },
-    handler(argv) {
-        notes.remove(argv.title);
-    }
+  },
+  handler(argv) {
+    notes.remove(argv.title);
+  },
 });
 
 yargs.command({
-    command: 'list',
-    describe: 'Lists all the notes present to the console',
-    handler(argv) {
-        notes.list();
-    }
+  command: "list",
+  describe: "Lists all the notes present to the console",
+  handler(argv) {
+    notes.list();
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note of the specified title",
+  builder: {
+    title: {
+      describe: "title to read",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.read(argv.title);
+  },
 });
 
 yargs.parse();
--- notes.js
+++ notes.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-const chalk = require('chalk');
+const fs = require("fs");
+const chalk = require("chalk");
+const { timeLog } = require("console");
 
 const addNote = (title, body) => {
   const notes = getAllNotes();
 
-  let serchTitle = notes.find((note) => {
+  const searchTitle = notes.find((note) => {
     return note.title === title;
   });
-  if (serchTitle != undefined) {
+  if (searchTitle) {
     console.log(chalk.red("Title already present"));
     return;
   }
@@ -17,30 +18,41 @@ const addNote = (title, body) => {
     body: body,
   });
   saveNotes(notes);
-  console.log(chalk.green('Note added successfully'));
+  console.log(chalk.green("Note added successfully"));
 };
 
 const removeNote = (title) => {
-    const notes = getAllNotes();
-    const duplicateNotes = notes.slice();
-    const modifiedNotes = duplicateNotes.filter((note) => {
-        return note.title !== title;
-    });
+  const notes = getAllNotes();
+  const duplicateNotes = notes.slice();
+  const modifiedNotes = duplicateNotes.filter((note) => {
+    return note.title !== title;
+  });
 
-    let msg = chalk.green('Note removed');
-    if (duplicateNotes.length == modifiedNotes.length)
-        msg = chalk.red('Title not found');
-    console.log(msg);
+  let msg = chalk.green("Note removed");
+  if (duplicateNotes.length == modifiedNotes.length)
+    msg = chalk.red("Title not found");
+  console.log(msg);
 
-    saveNotes(modifiedNotes);
-}
+  saveNotes(modifiedNotes);
+};
 
 const listNotes = () => {
-  console.log(chalk.bold.green('Your Notes'));
+  console.log(chalk.bold.green("Your Notes"));
   const notes = getAllNotes();
   notes.forEach((note) => {
     console.log(note.title);
   });
+};
+
+const readNotes = (title) => {
+  const notes = getAllNotes();
+  const note = notes.find((note) => note.title === title);
+  if (!note) {
+    console.log(chalk.red('No note found'));
+    return;
+  }
+  console.log(chalk.green('Note found'));
+  console.log(chalk.bold(note.title), note.body);
 }
 
 const getAllNotes = () => {
@@ -62,4 +74,5 @@ module.exports = {
   add: addNote,
   remove: removeNote,
   list: listNotes,
+  read: readNotes,
 };
